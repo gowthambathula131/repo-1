ec33e55ffc3a128190960f5f7e9ba0f1531efbcd
Gowtham <Gowtham@LAPTOP-Q52B7V29>
2020-03-29 11:44:39 +0530

    this is a trail project

---
+++ Trailprog/src/main/java/com/cg/doom/FirstClass.java
@@ -0,0 +1,10 @@
+package com.cg.doom;
+
+public class FirstClass {
+
+	public void print( ) {
+		// TODO Auto-generated method stub
+System.out.println("this is a sample code for git hub");
+	}
+
+}
---
+++ Trailprog/src/main/java/com/cg/zoom/MainClass.java
@@ -0,0 +1,13 @@
+package com.cg.zoom;
+
+import com.cg.doom.FirstClass;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		FirstClass fc = new FirstClass ();
+		fc.print();
+	}
+
+}
