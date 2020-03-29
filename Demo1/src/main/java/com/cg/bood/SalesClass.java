5ea26c539c6fa43da1e262a877f3c9b2266dd7ee
Gowtham <Gowtham@LAPTOP-Q52B7V29>
2020-03-29 10:48:35 +0530

    this is a small code

---
+++ Demo1/src/main/java/com/cg/bood/SalesClass.java
@@ -0,0 +1,8 @@
+package com.cg.bood;
+
+public class SalesClass {
+	public void print()
+	{
+		System.out.println("satya");
+	}
+}
---
+++ Demo1/src/main/java/com/cg/mood/MainClass.java
@@ -0,0 +1,11 @@
+package com.cg.mood;
+
+import com.cg.bood.SalesClass;
+
+public class MainClass {
+public static void main(String args[])
+{
+	SalesClass sc = new SalesClass();
+	sc.print();
+}
+}
